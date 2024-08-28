85b955b190992c28ff6859934a853d96755a22c7
lzzzzy <lzy1522659928@mail.ustc.edu.cn>
2024-08-28 09:38:27 +0000

    封装

--- chat/model.py
+++ chat/model.py
@@ -3,36 +3,33 @@ from mindnlp.core import no_grad
 from mindnlp.transformers import AutoModelForCausalLM, AutoTokenizer
 from mindspore._c_expression import _framework_profiler_step_start
 from mindspore._c_expression import _framework_profiler_step_end
-# 加载tokenizer文件
-# tokenizer = AutoTokenizer.from_pretrained("ZhipuAI/glm-4-9b-chat", mirror='modelscope')
-tokenizer = AutoTokenizer.from_pretrained("THUDM/glm-4-9b-chat",trust_remote_code=True)
-# tokenizer = AutoTokenizer.from_pretrained("ZhipuAI/glm-4-9b-chat",trust_remote_code=True)
-# tokenizer = AutoTokenizer.from_pretrained("ZhipuAI/glm-4-9b-chat")
 
-# 在此输入希望和ChatGLM4对话的内容
-query = "你好"
+class chat_model:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("THUDM/glm-4-9b-chat",trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "THUDM/glm-4-9b-chat",
+            ms_dtype=mindspore.float16,
+            # mirror='modelscope',
+            # trust_remote_code=True
+        ).eval()
+        print("*****Load Complete*****")
 
-inputs = tokenizer.apply_chat_template([{"role": "user", "content": query}],
-                                       add_generation_prompt=True,
-                                       tokenize=True,
-                                       return_tensors="ms",
-                                       return_dict=True
-                                       )
-print(inputs)
-# 加载模型
-model = AutoModelForCausalLM.from_pretrained(
-    # "ZhipuAI/glm-4-9b-chat",
-    "THUDM/glm-4-9b-chat",
-    # mirror='modelscope',
-    ms_dtype=mindspore.float16,
-    # trust_remote_code=True
-).eval()
+    def query(self, question:str):
+        inputs = self.tokenizer.apply_chat_template(
+            [{"role": "user", "content": question}],
+            add_generation_prompt=True,
+            tokenize=True,
+            return_tensors="ms",
+            return_dict=True
+        )
+        gen_kwargs = {"max_length": 100, "do_sample": True, "top_k": 1}
+        with no_grad():
+            outputs = self.model.generate(**inputs, **gen_kwargs)
+            outputs = outputs[:, inputs['input_ids'].shape[1]:]
+            return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-# 设置采样、最大生成长度等配置，开始推理
-# _framework_profiler_step_start()
-gen_kwargs = {"max_length": 100, "do_sample": True, "top_k": 1}
-with no_grad():
-    outputs = model.generate(**inputs, **gen_kwargs)
-    outputs = outputs[:, inputs['input_ids'].shape[1]:]
-    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-# _framework_profiler_step_end()
+if __name__ == '__main__':
+    model = chat_model()
+    print(model.query("你好"))
+        
