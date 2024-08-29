b3bf16eea44f0a789c0bffb6a3e74be7e45c67fe
Hongzhi Mao <hongzhimao@smail.nju.edu.cn>
2024-08-29 17:35:32 +0800

    [server] v1

--- chat/model.py
+++ chat/model.py
@@ -65,6 +65,20 @@ class chat_model:
             return response
 
 
+    def direct_query(self, question: str):
+        inputs = self.tokenizer.apply_chat_template(
+            [{"role": "user", "content": question}],
+            add_generation_prompt=True,
+            tokenize=True,
+            return_tensors="ms",
+            return_dict=True
+        )
+        gen_kwargs = {"max_length": 4100, "do_sample": True, "top_k": 1}
+        with no_grad():
+            outputs = self.model.generate(**inputs, **gen_kwargs)
+            outputs = outputs[:, inputs['input_ids'].shape[1]:]
+            return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
 if __name__ == '__main__':
     model = chat_model()
     print(model.query("如何求解一个矩阵的逆"))
